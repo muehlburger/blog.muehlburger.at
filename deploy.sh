dbc4af3ee9f518555582cf6deb52776e358d9f91
Herbert Mühlburger <mail@muehlburger.at>
2020-04-11 08:49:35 +0200

    Rebuilding site at Sa 11 Apr 2020 08:49:35 CEST

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,15 @@ git push origin master
 
 # Come Back up to the Project Root
 cd ..
+
+git add .
+
+# Commit changes.
+msg="Rebuilding site at `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
